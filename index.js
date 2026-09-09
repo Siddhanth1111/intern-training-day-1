ec743b6e4ec57aa7444d6768accb71771162f60f
Siddhanth1111 <siddhanthchauhan11@gmail.com>
2026-09-09 17:54:52 +0530

    first commit on br2

--- index.js
+++ index.js
@@ -18,11 +18,17 @@ app.get('/todos', (req, res) => {
   res.json(todos);
 });
 
-// app.put('/update/:id', (req, res) => {
-//   const id = req.params.id;
-//   const body = req.body;
-//   todos[id] = body;
-//   res.json({message: 'Todo updated successfully'});
-// });
+app.put('/update/:id', (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  todos[id] = body;
+  res.json({message: 'Todo updated successfully'});
+});
+
+app.delete('/delete/:id', (req, res) => {
+  const id = req.params.id;
+  todos.splice(id, 1);
+  res.json({message: 'Todo deleted successfully'});
+});
 
 app.listen(3000);
