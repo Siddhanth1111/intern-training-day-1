bedfe72730292c7d51bd30fb0d77ba8a8cc2f1d5
Siddhanth1111 <siddhanthchauhan11@gmail.com>
2026-09-09 18:07:10 +0530

    change in br2

--- index.js
+++ index.js
@@ -25,10 +25,6 @@ app.put('/update/:id', (req, res) => {
   res.json({message: 'Todo updated successfully'});
 });
 
-app.delete('/delete/:id', (req, res) => {
-  const id = req.params.id;
-  todos.splice(id, 1);
-  res.json({message: 'Todo deleted successfully'});
-});
+
 
 app.listen(3000);
